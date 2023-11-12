5f7ec3522de2486d789bc5aab21926dd3d47e8f8
Edoardo Ceresi <edoardo.ceresi@studio.unibo.it>
2023-11-12 13:36:25 +0100

    i'v done the point 4,5,6

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,7 +1,10 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Example class using {@link List} and {@link Map}.
@@ -9,7 +12,14 @@ import java.util.Map;
  */
 public final class UseListsAndMaps {
 
+    private static final int  ELEMENT = 100000;
     private UseListsAndMaps() {
+    }
+
+    private static void printForEach(List<Integer> numberList){
+        for(Integer element: numberList){
+            System.out.println(element);
+        }
     }
 
     /**
@@ -21,19 +31,58 @@ public final class UseListsAndMaps {
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
+        List<Integer> numberList = new ArrayList<Integer>();
+        long timeArray = System.nanoTime();
+
+        for(int i = 0; i <ELEMENT; i++){
+            int numbetToAdd = 1000 + i;
+            numberList.add(Integer.valueOf(numbetToAdd));
+        }
+
+        timeArray = System.nanoTime() - timeArray;
+        final var millis = TimeUnit.NANOSECONDS.toMillis(timeArray);
+        System.out.println(// NOPMD
+            "Add "
+                + numberList.size()
+                + " into an arrayList took "
+                + timeArray
+                + "ns ("
+                + millis
+                + "ms)"
+        );
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+        long timeLinked = System.nanoTime();
+        List<Integer> numberLinkedList = new LinkedList<Integer>(numberList);
+
+        timeLinked = System.nanoTime() - timeLinked;
+        final var millisLink = TimeUnit.NANOSECONDS.toMillis(timeLinked);
+        System.out.println(// NOPMD
+                "Add "
+                + numberList.size()
+                + " into an arrayList took "
+                + timeLinked
+                + "ns ("
+                + millisLink
+                + "ms)"
+        );
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+        int tmp = numberList.get(numberList.size()-1);
+        numberList.set(numberList.size()-1,numberList.get(0));
+        numberList.set(0,tmp);
+
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
+        //printForEach(numberList);
+
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
@@ -41,12 +90,40 @@ public final class UseListsAndMaps {
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
          */
+
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
          * LinkedList, using the collections of point 5. In order to measure
          * times, use as example TestPerformance.java.
          */
+        timeArray = System.nanoTime();
+        for(int j=0; j < 1000; j++){
+            System.out.println(numberList.get(( numberList.size()/2 ) + j));
+        }
+        timeArray = System.nanoTime() - timeArray;
+        final var milliArray = TimeUnit.NANOSECONDS.toMillis(timeArray);
+
+        timeLinked = System.nanoTime();
+        for(int j=0; j < 1000; j++){
+            System.out.println(numberLinkedList.get(( numberList.size()/2 ) + j));
+        }
+        timeLinked = System.nanoTime() - timeLinked;
+        final var milliLinked = TimeUnit.NANOSECONDS.toMillis(timeArray);
+        System.out.println(// NOPMD
+            "to read 1000 into the middle to an ArrayList took "
+                + timeArray
+                + "ns ("
+                + milliArray
+                + "ms)"
+        );
+        System.out.println(// NOPMD
+            "to read 1000 into the middle to an LinkedList took "
+                + timeArray
+                + "ns ("
+                + milliLinked
+                + "ms)"
+        );
         /*
          * 7) Build a new Map that associates to each continent's name its
          * population:
